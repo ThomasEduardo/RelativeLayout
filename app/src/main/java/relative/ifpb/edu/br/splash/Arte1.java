1afb28fffdace455e365160e4abb912ce43b3464
Thomas Eduardo <thomaseduu@gmail.com>
2016-08-15 17:49:43 -0300

    Update Arte1.java

--- app/src/main/java/relative/ifpb/edu/br/splash/Arte1.java
+++ app/src/main/java/relative/ifpb/edu/br/splash/Arte1.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import com.coderefer.androidsplashscreenexample.R;
 
 /**
- * Created by Diego A. Gama on 29/07/2016.
+ * Created by Diego A. Gama on 29/07/2016. copiei o comentario"autor" dele pq esqueci como fazia e acabei colocando tudo sem querer kkkk
  */
 public class Arte1 extends Activity {
 
