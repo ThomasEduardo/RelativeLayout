807bfc2a5fe0439196e2191a70150591ba76eda7
Thomas Eduardo <thomaseduu@gmail.com>
2016-08-15 17:51:58 -0300

    Update Z1.java

--- app/src/main/java/relative/ifpb/edu/br/splash/Z1.java
+++ app/src/main/java/relative/ifpb/edu/br/splash/Z1.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.coderefer.androidsplashscreenexample.R;
-
+/**
+ * Created by Lorena, Sarah and Thomas on 01/07/2016.
+ */
 public class Z1 extends Activity {
 
     @Override
